f937ef3bceeba2e1a3042f4097ae6b11827fff31
Nitin Chaudhary <nitin.io@outlook.com>
2023-04-30 14:32:05 +0530

    Add: Autoplay Button

--- src/components/ImageAndDetails.tsx
+++ src/components/ImageAndDetails.tsx
@@ -1,8 +1,6 @@
-import React, { useState } from "react";
-import { Grid, Typography } from "@mui/material";
+import React from "react";
+import { Grid } from "@mui/material";
 import NavigationBar from "./NavigationBar";
-import PlayCircleIcon from "@mui/icons-material/PlayCircle";
-import { PauseCircleFilled } from "@mui/icons-material";
 
 interface Props {
   title: string;
@@ -11,43 +9,20 @@ interface Props {
 }
 
 const ImageAndDetails: React.FC<Props> = ({ title, content, imageURL }) => {
-  const [slideShow, setSlideShow] = useState<boolean>(false);
-
   return (
     <>
       <Grid item md={8}>
-        <Grid item className="image-container">
+        <div className="image-container">
           <img src={imageURL} />
-        </Grid>
-        <NavigationBar />
+        </div>
       </Grid>
       <Grid item md={4}>
         <Grid item className="detail-container">
           <h1>{title}</h1>
           <p>{content}</p>
         </Grid>
-        <Grid
-          item
-          md={12}
-          justifyContent="center"
-          alignItems="center"
-          className="play-btn-container"
-        >
-          <Typography align="center">
-            {slideShow ? (
-              <PauseCircleFilled
-                sx={{ fontSize: 60, color: "#25BEDA" }}
-                onClick={() => setSlideShow(!slideShow)}
-              />
-            ) : (
-              <PlayCircleIcon
-                sx={{ fontSize: 60, color: "#25BEDA" }}
-                onClick={() => setSlideShow(!slideShow)}
-              />
-            )}
-          </Typography>
-        </Grid>
       </Grid>
+      <NavigationBar />
     </>
   );
 };
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import data from "../assets/data.js";
 import { useCatalogContext } from "./CatalogContext.js";
+import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import { PauseCircleFilled } from "@mui/icons-material";
 
 interface Post {
   title: string;
@@ -13,17 +15,7 @@ interface Post {
 const NavigationBar: React.FC = () => {
   const [posts, setPosts] = useState<Post[]>([]);
   const { setActiveCatalog } = useCatalogContext();
-
-  const imageStyling = {
-    width: "150%",
-    height: "5rem",
-    margin: "0 0.4rem",
-    backgroundPosition: "center",
-    backgroundRepeat: "noRepeat",
-    backgroundSize: "cover",
-    border: "none",
-    borderRadius: "0.5rem",
-  };
+  const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
     setPosts(data);
@@ -45,9 +37,24 @@ const NavigationBar: React.FC = () => {
     setActiveCatalog(posts[Math.floor(posts.length / 2)]);
   }, [posts]);
 
+  useEffect(() => {
+    let interval: NodeJS.Timeout | unknown = null;
+    if (isPlaying) {
+      interval = setInterval(() => {
+        handleNext();
+        console.log("playing");
+      }, 3000);
+    } else {
+      clearInterval(interval as NodeJS.Timeout);
+    }
+    return () => {
+      clearInterval(interval as NodeJS.Timeout);
+    };
+  }, [isPlaying]);
+
   return (
     <>
-      <Grid item>
+      <Grid item md={8} xs={12}>
         <Grid
           container
           justifyContent={"center"}
@@ -67,18 +74,40 @@ const NavigationBar: React.FC = () => {
                 md={2}
                 xs={1}
                 style={{
-                  ...imageStyling,
+                  margin: "0 0.45rem",
                   backgroundImage: `url(  ${item.imageURL})`,
                 }}
                 onClick={() => {
                   setActiveCatalog(item);
                 }}
-                className="btn"
+                className="nav-btn"
               ></Grid>
             );
           })}
           <PlayArrowIcon onClick={handleNext} className="navigation-btn" />
         </Grid>
+      </Grid>
+      <Grid
+        item
+        md={4}
+        xs={12}
+        justifyContent="center"
+        alignItems="center"
+        className="play-btn-container"
+      >
+        <Typography align="center">
+          {isPlaying ? (
+            <PauseCircleFilled
+              sx={{ fontSize: 60, color: "#25BEDA" }}
+              onClick={() => setIsPlaying(!isPlaying)}
+            />
+          ) : (
+            <PlayCircleIcon
+              sx={{ fontSize: 60, color: "#25BEDA" }}
+              onClick={() => setIsPlaying(!isPlaying)}
+            />
+          )}
+        </Typography>
       </Grid>
     </>
   );
